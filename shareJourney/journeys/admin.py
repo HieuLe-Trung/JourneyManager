1ecdf69bd7103f71120c2a2be6dc11e82c086fe4
Your name <mtrunghieu12@gmail.com>
2024-04-10 23:52:05 +0700

    update models và thêm report

--- shareJourney/journeys/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Journey,Checkpoint
-
-
-# Register your models here.
-admin.site.register(Journey)
-admin.site.register(Checkpoint)
--- shareJourney/journeys/models.py
+++ shareJourney/journeys/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
 
 class Journey(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # người tạo hành trình
+    name = models.CharField(max_length=100, blank=False,null=False,default='')
     start_location = models.CharField(max_length=100)
     end_location = models.CharField(max_length=100)
     created_date = models.DateTimeField(auto_now_add=True)
@@ -16,7 +17,7 @@ class Journey(models.Model):
     active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.user.username}'s journey from {self.start_location} to {self.end_location}"
+        return self.name
 
 
 class Interaction(models.Model):
@@ -27,8 +28,8 @@ class Interaction(models.Model):
         abstract = True
 
 
-class Checkpoint(models.Model):
-    journey = models.ForeignKey(Journey, related_name='checkpoints', on_delete=models.CASCADE)
+class VisitPoint(models.Model):
+    journey = models.ForeignKey(Journey, related_name='visit_point', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)  # 1 hành trình có nhieu điểm ghé qua
     estimated_duration_to_destination = models.DurationField(null=True,
                                                              blank=True)  # thời gian ước tính từ điểm hiện tại đến đích
@@ -37,13 +38,10 @@ class Checkpoint(models.Model):
         return self.name
 
 
-class Participation(Interaction):  # tạo ra bảng mới giữa User-journey
+class Participation(Interaction):  # ds user tham gia hành trình
     joined_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(blank=True, null=True)
     is_approved = models.BooleanField(default=False)  # xác nhận người tham gia hành trình
-
-    def __str__(self):
-        return f"{self.user.username} joined {self.journey} at {self.joined_at}"
+    rating = models.IntegerField(null=True,blank=True)
 
 
 class Post(Interaction):
@@ -51,23 +49,32 @@ class Post(Interaction):
     image = models.ImageField(upload_to='postImageJourney', null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     lock_cmt = models.BooleanField(default=False)  # đóng khi đã đủ người tham gia
-
-    def __str__(self):
-        return f"Post by {self.user.username} created at {self.created_date}"
+    visit_point = models.OneToOneField(VisitPoint, related_name='post', on_delete=models.CASCADE, null=True, blank=True)
 
 
-class Like(Interaction):
-    class Meta:
-        unique_together = ("journey", "user")
-
-
-class Comment(models.Model):
+class InteractionPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,default='')
+
+    class Meta:
+        abstract = True
+
+
+class Like(InteractionPost):
+    class Meta:
+        unique_together = ("post", "user")
+
+
+class Comment(InteractionPost):
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    is_approved = models.BooleanField(default=False)  # duyệt người tham gia
+    is_approved = models.BooleanField(default=False)  # duyệt người tham gia(tick)
+    # khi được duyệt thì true, đưa người dùng đó vào participation và cũng chỉnh approved=True
 
-    def __str__(self):
-        return f"Comment by {self.user.username} on post {self.post} at {self.created_date}"
+
+class Report(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(User, related_name='reports_given', on_delete=models.CASCADE)
+    reason = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
