2d9043d7dc558a6f62ac51c4ac8a7c1915f7e4a3
Your name <mtrunghieu12@gmail.com>
2024-06-14 17:28:52 +0700

    fix model Post

--- shareJourney/journeys/models.py
+++ shareJourney/journeys/models.py
@@ -52,8 +52,8 @@ class Participation(Interaction):  # ds user tham gia hành trình
 class Post(Interaction):
     content = models.TextField()
     visit_point = models.CharField(max_length=255, blank=True, null=True)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
     estimated_time_of_arrival = models.CharField(max_length=100, blank=True, null=True)
 
 
